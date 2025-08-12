34d88b7bb66b414db4b4b21f9ff29b6abe5f3940
Роман Мархаток < .@mgw.local>
2025-08-12 15:46:54 +0300

    was create warehouses model

--- ws/db/models/__init__.py
+++ ws/db/models/__init__.py
@@ -1,3 +1,4 @@
 from .roles import Roles  # noqa
 from .users import Users  # noqa
 from .base import BaseModel  # noqa
+from .warehouses import Warehouses  # noqa
---
+++ ws/db/models/warehouses.py
@@ -0,0 +1,18 @@
+import uuid
+from .base import BaseModel
+from sqlalchemy import UUID, ForeignKey, String
+from sqlalchemy.orm import mapped_column, Mapped
+
+
+class Warehouses(BaseModel):
+    __tablename__ = "warehouses"
+    warehouse_name: Mapped[str] = mapped_column(String(200), unique=True, index=True)
+    warehouse_worker_uuididf: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey(
+            "users.uuididf",
+            onupdate="CASCADE",
+            ondelete="SETNULL",
+        ),
+        nullable=False,
+    )
