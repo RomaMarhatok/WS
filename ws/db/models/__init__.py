ce1c8cd786b7d09d20e349c99c4aeae9bc2b42bf
roma <roma.marhatok@yandex.by>
2025-08-12 21:18:46 +0300

    was create items table

--- ws/db/models/__init__.py
+++ ws/db/models/__init__.py
@@ -3,3 +3,4 @@ from .users import Users  # noqa
 from .base import BaseModel  # noqa
 from .warehouses import Warehouses  # noqa
 from .item_types import ItemTypes  # noqa
+from .items import Items  # noqa
