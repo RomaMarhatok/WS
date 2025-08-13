fde6e22508cc8bb24b64d84b3f6627de1df199bb
Роман Мархаток < .@mgw.local>
2025-08-13 11:56:33 +0300

    set as_uuid=True in orders model

--- ws/db/models/orders.py
+++ ws/db/models/orders.py
@@ -7,25 +7,25 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Orders(BaseModel):
     __tablename__ = "orders"
     status_uuididf: Mapped[uuid.UUID] = mapped_column(
-        UUID,
+        UUID(as_uuid=True),
         ForeignKey("order_statuses.uuididf", onupdate="CASCADE", ondelete="SET NULL"),
         nullable=True,
         index=True,
     )
     item_uuididf: Mapped[uuid.UUID] = mapped_column(
-        UUID,
+        UUID(as_uuid=True),
         ForeignKey("items.uuididf", onupdate="CASCADE", ondelete="SET NULL"),
         nullable=True,
         index=True,
     )
     warehouse_uuididf: Mapped[uuid.UUID] = mapped_column(
-        UUID,
+        UUID(as_uuid=True),
         ForeignKey("warehouses.uuididf", onupdate="CASCADE", ondelete="SET NULL"),
         nullable=True,
         index=True,
     )
     customer_uuididf: Mapped[uuid.UUID] = mapped_column(
-        UUID,
+        UUID(as_uuid=True),
         ForeignKey("users.uuididf", onupdate="CASCADE", ondelete="SET NULL"),
         nullable=True,
         index=True,
