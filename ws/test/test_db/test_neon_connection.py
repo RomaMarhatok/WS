c4bcdeeba7e8736fe61c0205b690e96fb957f5b9
Роман Мархаток < .@mgw.local>
2025-08-15 15:26:47 +0300

    remove unused print

--- ws/test/test_db/test_neon_connection.py
+++ ws/test/test_db/test_neon_connection.py
@@ -6,7 +6,6 @@ from ws.db.session import get_sqlalchemy_async_engine
 @pytest.mark.asyncio
 async def test_create_sqlalchemy_engine_for_neon_db():
     engine = get_sqlalchemy_async_engine()
-    print(engine.url)
     async with engine.connect() as conn:
         result = await conn.execute(text("select 'HELLO WORLD'"))
         s = result.scalar()
