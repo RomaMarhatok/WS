871c19051f8182df0b0c93eb27eec6057fe83607
roma <roma.marhatok@yandex.by>
2025-08-17 17:14:04 +0300

    the method implementation has been moved from child repositories to the base repository

--- ws/db/repository/base_repository.py
+++ ws/db/repository/base_repository.py
@@ -1,36 +1,77 @@
 import uuid
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import Generic
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy import select, update
+from sqlalchemy.exc import IntegrityError
+
 from ws.db.types import SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE
+
 from ws.db.session import get_session_factory
+from ws.db.exceptions import EntityNotFoundException, EntityAlreadyExistException
 
 
 class GenericRepository(ABC, Generic[SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE]):
 
-    def __init__(
-        self, session_factory: async_sessionmaker[AsyncSession] = get_session_factory()
-    ):
-        self.session_factory = session_factory
+    def __init__(self):
+        self.session_factory = get_session_factory()
 
     @property
     def _model(self) -> SQLALCHEMY_MODEL_TYPE:
         raise NotImplementedError
 
-    @abstractmethod
     async def save(self, dto: PYDANTIC_SCHEMA_TYPE) -> SQLALCHEMY_MODEL_TYPE:
-        raise NotImplementedError
+        async with self.session_factory() as session:
+            try:
+                entity = self._model(**dto.model_dump())
+                session.add(entity)
+                await session.commit()
+                await session.refresh(entity)
+                return entity
+            except IntegrityError as exc:
+                await session.rollback()
+                raise EntityAlreadyExistException(exc.orig)
+            except Exception as exc:
+                await session.rollback()
+                raise exc
 
-    @abstractmethod
-    async def get_by_uuididf(self, uuididf: uuid) -> SQLALCHEMY_MODEL_TYPE:
-        raise NotImplementedError
+    async def get_by_uuididf(self, uuididf: uuid.UUID) -> SQLALCHEMY_MODEL_TYPE:
+        async with self.session_factory() as session:
+            q = select(self._model).where(self._model.uuididf == uuididf)
+            entity = (await session.execute(q)).scalar_one_or_none()
+            if entity is None:
+                raise EntityNotFoundException(
+                    f"Entity {self._model.__name__} with UUID {uuididf} not found"
+                )
+            return entity
 
-    @abstractmethod
-    async def update(
-        self, old_instance: SQLALCHEMY_MODEL_TYPE, new_dto: PYDANTIC_SCHEMA_TYPE
-    ):
-        raise NotImplementedError
+    async def update(self, dto: PYDANTIC_SCHEMA_TYPE) -> SQLALCHEMY_MODEL_TYPE:
+        async with self.session_factory() as session:
+            stmt = (
+                update(self._model)
+                .where(self._model.uuididf == dto.uuididf)
+                .values(**dto.model_dump())
+                .returning(self._model)
+            )
+            entity = (await session.execute(stmt)).scalar_one_or_none()
 
-    @abstractmethod
-    async def delete(self, uuididf: uuid) -> bool:
-        raise NotImplementedError
+            if entity is None:
+                raise EntityNotFoundException(
+                    f"Entity {self._model.__name__} with UUID {dto.uuididf} not found"
+                )
+            await session.commit()
+            return entity
+
+    async def delete(self, uuididf: uuid.UUID) -> None:
+        async with self.session_factory() as session:
+            q = select(self._model).where(self._model.uuididf == uuididf)
+            entity = (await session.execute(q)).scalar_one_or_none()
+            if entity is None:
+                raise EntityNotFoundException(
+                    f"Entity {self._model.__name__} with uUUID {uuididf} not found"
+                )
+            await session.delete(entity)
+            await session.commit()
+
+    async def get_batch(self) -> list[SQLALCHEMY_MODEL_TYPE]:
+        async with self.session_factory() as session:
+            return (await session.execute(select(self._model))).scalars().all()
