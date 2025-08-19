c8e6b8937c247d52c733fda17e2c0b21550b81f7
roma <roma.marhatok@yandex.by>
2025-08-19 19:29:49 +0300

    add new envirom var into conftest

--- ws/test/conftest.py
+++ ws/test/conftest.py
@@ -2,12 +2,14 @@ import os
 import sys
 import asyncio
 import pytest_asyncio
+from dotenv import load_dotenv
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     AsyncEngine,
 )
 
+load_dotenv(override=True)
 if sys.platform.startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -15,6 +17,11 @@ if sys.platform.startswith("win"):
 @pytest_asyncio.fixture(scope="session")
 async def neon_db_url() -> str:
     return os.environ["NEON_DB_URL"]
+
+
+@pytest_asyncio.fixture(scope="session")
+async def neon_db_url_for_test():
+    return os.environ["NEON_TEST_URL"]
 
 
 @pytest_asyncio.fixture(scope="session")
