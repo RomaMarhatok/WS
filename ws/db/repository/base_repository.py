a399a24e6f21ab5809e22de690224c70a1048943
roma <roma.marhatok@yandex.by>
2025-08-24 10:06:58 +0300

    implement get method for only one field

--- ws/db/repository/base_repository.py
+++ ws/db/repository/base_repository.py
@@ -1,11 +1,11 @@
 import uuid
-from abc import ABC
 from functools import lru_cache
 from typing import Generic, get_args
 from sqlalchemy import select, update, Select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from asyncpg.exceptions import ForeignKeyViolationError, UniqueViolationError
+from abc import ABC
 from ws.db.types import SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE
 from ws.db.exceptions import (
     EntityNotFoundException,
@@ -15,16 +15,16 @@ from ws.db.exceptions import (
 )
 
 
-class GenericRepository(ABC, Generic[SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE]):
+class GenericRepository(Generic[SQLALCHEMY_MODEL_TYPE], ABC):
 
     def __init__(self, session: AsyncSession):
-        self._model, self._dto = self._get_entity_classes()
+        self.model = self._get_entity_classes()
         self.session = session
 
     async def save(self, dto: PYDANTIC_SCHEMA_TYPE) -> SQLALCHEMY_MODEL_TYPE:
         async with self.session:
             try:
-                entity = self._model(**dto.model_dump())
+                entity = self.model(**dto.model_dump())
                 self.session.add(entity)
                 await self.session.commit()
                 await self.session.refresh(entity)
@@ -40,24 +40,14 @@ class GenericRepository(ABC, Generic[SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE
                     raise EntityAlreadyExistException(error_msg)
                 raise CouldNotCreateEntityException from exc
 
-    async def get_by_uuididf(self, uuididf: uuid.UUID) -> SQLALCHEMY_MODEL_TYPE:
-        async with self.session:
-            q = Select(self._model).where(self._model.uuididf == uuididf)
-            entity = (await self.session.execute(q)).scalar_one_or_none()
-            if entity is None:
-                raise EntityNotFoundException(
-                    f"Entity {self._model.__name__} with UUID {uuididf} not found"
-                )
-            return entity
-
     async def update(self, dto: PYDANTIC_SCHEMA_TYPE) -> SQLALCHEMY_MODEL_TYPE:
         async with self.session:
             try:
                 stmt = (
-                    update(self._model)
-                    .where(self._model.uuididf == dto.uuididf)
+                    update(self.model)
+                    .where(self.model.uuididf == dto.uuididf)
                     .values(**dto.model_dump())
-                    .returning(self._model)
+                    .returning(self.model)
                 )
                 entity = (await self.session.execute(stmt)).scalar_one_or_none()
             except IntegrityError as exc:
@@ -69,53 +59,49 @@ class GenericRepository(ABC, Generic[SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE
                     raise ForeignKeyNotExist(error_msg)
             if entity is None:
                 raise EntityNotFoundException(
-                    f"Entity {self._model.__name__} with UUID {dto.uuididf} not found"
+                    f"Entity {self.model.__name__} with UUID {dto.uuididf} not found"
                 )
             await self.session.commit()
             return entity
 
     async def delete(self, uuididf: uuid.UUID) -> None:
         async with self.session:
-            q = Select(self._model).where(self._model.uuididf == uuididf)
+            q = Select(self.model).where(self.model.uuididf == uuididf)
             entity = (await self.session.execute(q)).scalar_one_or_none()
             if entity is None:
                 raise EntityNotFoundException(
-                    f"Entity {self._model.__name__} with UUID {uuididf} not found"
+                    f"Entity {self.model.__name__} with UUID {uuididf} not found"
                 )
             await self.session.delete(entity)
             await self.session.commit()
 
     async def get_batch(self) -> list[SQLALCHEMY_MODEL_TYPE]:
         async with self.session:
-            return (await self.session.execute(select(self._model))).scalars().all()
+            return (await self.session.execute(select(self.model))).scalars().all()
 
-    async def get_by_name(self, name: str, name_field=None) -> SQLALCHEMY_MODEL_TYPE:
-        entity_name_field = (
-            name_field if name_field is not None else self._get_entity_name_field()
-        )
-        async with self.session:
-            stmt = Select(self._model).where(entity_name_field == name)
-            entity = (await self.session.execute(stmt)).scalar_one_or_none()
-            if entity is None:
-                raise EntityNotFoundException(
-                    f"Entity {self._model.__name__} with name {name} not found"
-                )
-            return entity
+    async def get(self, **kwargs) -> SQLALCHEMY_MODEL_TYPE:
+        if len(kwargs.items()) != 1:
+            raise ValueError(f"Method {self.get.__name__} accept only one argument")
+        field, value = list(kwargs.items())[0]
+        try:
+            async with self.session:
+                model_field = getattr(self.model, field)
+                stmt = Select(self.model).where(model_field == value)
+                entity = (await self.session.execute(stmt)).scalar_one_or_none()
+                if entity is None:
+                    raise EntityNotFoundException(
+                        f"Entity {self.model.__name__}"
+                        + f" with {model_field} == {value} not found"
+                    )
+                return entity
+        except AttributeError:
+            raise AttributeError(
+                f"Model {self.model.__name__} doesn't have {field} field"
+            )
 
     @classmethod
     @lru_cache(maxsize=1)
-    def _get_entity_classes(cls) -> tuple[SQLALCHEMY_MODEL_TYPE, PYDANTIC_SCHEMA_TYPE]:
+    def _get_entity_classes(cls) -> SQLALCHEMY_MODEL_TYPE:
         generic_types_of_repo = getattr(cls, "__orig_bases__")[0]
-        sqlachemy_entity_class, pydantic_entity_class = get_args(generic_types_of_repo)
-        return sqlachemy_entity_class, pydantic_entity_class
-
-    def _get_entity_name_field(self):
-        # find all fields wich contain word name
-        name_fields = list(
-            filter(lambda x: "__" not in x and "name" in x, dir(self._model))
-        )
-        if len(name_fields) == 0:
-            raise ValueError(
-                f"Entity {self._model} doesn't have field wich contain word 'name' "
-            )
-        return getattr(self._model, name_fields[0])
+        sqlachemy_entity_class = get_args(generic_types_of_repo)[0]
+        return sqlachemy_entity_class
