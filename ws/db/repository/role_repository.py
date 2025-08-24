0554fa53d34f2616676cd025d07c7321b40e0ea5
roma <roma.marhatok@yandex.by>
2025-08-24 10:06:38 +0300

    remove unused files

--- ws/db/repository/role_repository.py
+++
@@ -1,7 +0,0 @@
-from ws.db.repository.base_repository import GenericRepository
-from ws.db.models import Roles
-from ws.dto import RoleDTO
-
-
-class RoleRepository(GenericRepository[Roles, RoleDTO]):
-    pass
--- ws/db/repository/user_repository.py
+++
@@ -1,7 +0,0 @@
-from ws.db.models import Users
-from ws.dto import UserDTO
-from ws.db.repository.base_repository import GenericRepository
-
-
-class UserRepository(GenericRepository[Users, UserDTO]):
-    pass
