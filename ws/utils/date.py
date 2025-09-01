b553799f2ce7c9ea98a63ef4f5b3933d013e9269
RomaMarhatok <r.marhatok@yandex.by>
2025-09-01 10:20:45 +0300

    was implemented token service for auth service

--- ws/utils/date.py
+++ ws/utils/date.py
@@ -5,3 +5,7 @@ GMT_DATETIME_FORMAT = "%a, %d %b %Y %H:%M:%S GMT"
 
 def parse_datetime_to_gmt_format_str(time: datetime) -> str:
     return datetime.strftime(time, GMT_DATETIME_FORMAT)
+
+
+def parse_gmt_format_str_to_datetime(time: str) -> datetime:
+    return datetime.strptime(time, GMT_DATETIME_FORMAT)
