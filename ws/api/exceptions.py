d5cf6ab3c1a4ed720483bb848179c8e5956c1efc
RomaMarhatok <r.marhatok@yandex.by>
2025-09-01 10:20:59 +0300

    add new exceptions

--- ws/api/exceptions.py
+++ ws/api/exceptions.py
@@ -8,3 +8,11 @@ HTTP_409_CONFLICT_USERNAME_ALREADY_EXIST = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
     detail="This username already exist",
 )
+HTTP_401_REFRESH_TOKEN_EXPIRED = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Refresh token expired",
+)
+HTTP_404_RECORD_NOT_FOUND = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Records by that criteria doesn't found",
+)
